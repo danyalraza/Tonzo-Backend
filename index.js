4e7062c53005ff72b0d93e2bd11ea65f7d268215
Danyal Raza <smdraza@uwaterloo.ca>
2016-09-17 23:28:40 -0400

    eyy

--- index.js
+++ index.js
@@ -2,7 +2,10 @@ var express = require('express')
 var stormpath = require('express-stormpath')
 var pg = require('pg')
 var bodyParser = require('body-parser')
+var request = require('request');
 var app = express()
+var secrets = require('./secrets.');
+
 
 var config = {
   user: 'admin',
@@ -65,22 +68,18 @@ app.post('/users/:userid/analysis', stormpath.getUser, stormpath.loginRequired, 
         console.log(data.id);
         console.log(data.sentiment);
         // SQL Query > Select Data
-        var query = client.query("INSERT into moods(id, sentiment, date) values('$1, $2, current_date)", data.id, data.sentiment);
+        var query = client.query("INSERT into moods(id, sentiment, date) values('" + data.id + "'," + data.sentiment + ", current_date)");
 
-        // Stream results back one row at a time
-        query.on('row', function(row) {
-            results.push(row);
-        });
         // After all data is returned, close connection and return results
         query.on('end', function() {
             done();
-            return res.json(results);
+            return res.send("done");
         });
     });
   }
   else {
     console.log("401");
-    res.send("401");
+    return res.send("401");
   }
   // Get a Postgres client from the connection pool
 });
@@ -95,7 +94,6 @@ app.get('/users/:userid/analysis', stormpath.getUser, stormpath.loginRequired, f
           console.log(err);
           return res.status(500).json({ success: false, data: err});
         }
-
         // SQL Query > Select Data
         var query = client.query("SELECT id, avg(sentiment), date FROM moods group by id, date");
         // Stream results back one row at a time
@@ -113,7 +111,26 @@ app.get('/users/:userid/analysis', stormpath.getUser, stormpath.loginRequired, f
     console.log("401");
     res.send("401");
   }
-  // Get a Postgres client from the connection pool
+});
+
+app.get('/locations/find/:longitdue/:latitude/:keyword', stormpath.getUser, stormpath.loginRequired, function(req, res)  {
+  var location = {
+    'lat':req.params.longitude,
+    'lon':req.params.latitude
+  };
+  var radius = 20000;
+  var keyword = req.params.keyword;
+
+  var url = 'https://maps.googleapis.com/maps/api/place/nearbysearch/json?rankby=distance&location=' + location.lat + ',' + location.lon + '\&radius=' + radius + '\&keyword=' + keyword + '\&key=' + googleMapKey;
+  request(url , function (error, response, body) {
+    if (!error && response.statusCode == 200) {
+      var list = JSON.parse(body).results;
+      if (list.length == 0) return response.json({ success: true, data: "Nothing Returned"});
+      else {
+        return response.json(list[0]);
+      }
+    }
+  });
 });
 
 app.on('stormpath.ready', function () {
