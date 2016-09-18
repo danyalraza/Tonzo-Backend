0945a2a3593bfa7de105f44ad2a893048aeea7ab
Danyal Raza <smdraza@uwaterloo.ca>
2016-09-17 21:30:43 -0400

    add rutes

--- index.js
+++ index.js
@@ -1,9 +1,123 @@
 var express = require('express')
-
+var stormpath = require('express-stormpath')
+var pg = require('pg')
+var bodyParser = require('body-parser')
 var app = express()
 
-app.get('/notes', function(req, res) {
-  res.json({notes: "This is your notebook. Edit this to start saving your notes!"})
-})
+var config = {
+  user: 'admin',
+  host: 'localhost',
+  database: 'tonzo',
+  port: 26257
+};
 
-app.listen(3000)
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended: false}))
+
+
+app.use(stormpath.init(app, {
+  expand: {
+    customData: true,
+  },
+  web: {
+    produces: ['application/json']
+  }
+}))
+
+app.get('/users', stormpath.getUser, stormpath.loginRequired, function(req, res)  {
+  var results = [];
+  // Get a Postgres client from the connection pool
+  pg.connect(config, function(err, client, done) {
+      // Handle connection errors
+      if(err) {
+        done();
+        console.log(err);
+        return res.status(500).json({ success: false, data: err});
+      }
+
+      // SQL Query > Select Data
+      var query = client.query("SELECT * FROM sentimen ORDER BY id ASC");
+
+      // Stream results back one row at a time
+      query.on('row', function(row) {
+          results.push(row);
+      });
+
+      // After all data is returned, close connection and return results
+      query.on('end', function() {
+          done();
+          return res.json(results);
+      });
+  });
+});
+
+app.post('/users/:userid/analysis', stormpath.getUser, stormpath.loginRequired, function(req, res)  {
+  var results = [];
+  var data = {id: req.body.id, sentiment: req.body.sentiment};
+  if (req.params.userid == req.user.username){
+    pg.connect(config, function(err, client, done) {
+        // Handle connection errors
+        if(err) {
+          done();
+          console.log(err);
+          return res.status(500).json({ success: false, data: err});
+        }
+        console.log(data.id);
+        console.log(data.sentiment);
+        // SQL Query > Select Data
+        var query = client.query("INSERT into moods(id, sentiment, date) values('$1, $2, current_date)", data.id, data.sentiment);
+
+        // Stream results back one row at a time
+        query.on('row', function(row) {
+            results.push(row);
+        });
+        // After all data is returned, close connection and return results
+        query.on('end', function() {
+            done();
+            return res.json(results);
+        });
+    });
+  }
+  else {
+    console.log("401");
+    res.send("401");
+  }
+  // Get a Postgres client from the connection pool
+});
+
+app.get('/users/:userid/analysis', stormpath.getUser, stormpath.loginRequired, function(req, res)  {
+  var results = [];
+  if (req.params.userid == req.user.username){
+    pg.connect(config, function(err, client, done) {
+        // Handle connection errors
+        if(err) {
+          done();
+          console.log(err);
+          return res.status(500).json({ success: false, data: err});
+        }
+
+        // SQL Query > Select Data
+        var query = client.query("SELECT id, avg(sentiment), date FROM moods group by id, date");
+        // Stream results back one row at a time
+        query.on('row', function(row) {
+            results.push(row);
+        });
+        // After all data is returned, close connection and return results
+        query.on('end', function() {
+            done();
+            return res.json(results);
+        });
+    });
+  }
+  else {
+    console.log("401");
+    res.send("401");
+  }
+  // Get a Postgres client from the connection pool
+});
+
+app.on('stormpath.ready', function () {
+  console.log('Stormpath Ready');
+});
+
+app.listen(3000);
